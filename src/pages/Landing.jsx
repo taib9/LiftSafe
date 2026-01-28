aa9426565a750128451e5ca2a69239125b5dfe7e
kristy02ho <kristy02ho@gmail.com>
2026-01-27 20:24:55 -0700

    landing created

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,37 +1,48 @@
 import reactLogo from "../assets/react.svg";
 import viteLogo from "../../public/vite.svg";
-import "../../src/App.css";
-import { useState } from 'react'
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import heroImg from "../assets/Landing-Hero.png";
+import navLogo from "../assets/LiftSafe-Logo.png"
 
 const Landing = () => {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto flex flex-col items-center gap-10 py-4 lg:flex-row lg:justify-between">
+        <img src={navLogo} />
+        <div className="flex items-center gap-4">
+          <a href="" className="text-teal font-bold">
+            Login
+          </a>
+          <a
+            href=""
+            className="bg-teal font-bold tracking-wider text-white px-8 py-2 items-center flex rounded-full"
+          >
+            Sign Up
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container mx-auto mt-8 flex flex-col items-center gap-10 px-4 lg:flex-row lg:items-center lg:justify-betwee">
+        <div className="flex flex-col items-start sm:text-center sm:items-center lg:text-left">
+          <h1 className="text-teal leading-tight text-4xl sm:text-5xl lg:text-[5rem]">
+            <span className="font-bold">Protect</span> Your body.<br></br>
+            <span className="font-bold">Elevate</span> Your Performance
+          </h1>
+          <p className="text-2xl my-8">
+            Join us in protecting your body and elevating your performance
+            through prevention and strengthening.
+          </p>
+          <a
+            href=""
+            className="bg-teal font-bold tracking-wider text-white px-12 py-2 items-center flex rounded-full mx-auto" 
+          >
+            Get Started
+          </a>
+        </div>
+        <img src={heroImg} />
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
