9d029f31a3d7ec8ea3afec6c0e5cc4a2270b6204
tbaoiom <taib9642@gmail.com>
2026-01-29 16:11:13 -0700

    Added sliders and updated some styling

--- src/pages/PainQuestionsPage.jsx
+++ src/pages/PainQuestionsPage.jsx
@@ -55,20 +55,22 @@ const PainQuestionsPage = () => {
   };
 
   return (
-    <div>
-      <h1 className="text-center capitalize">{painAreaId}</h1>
-      <p className="text-teal font-bold text-center">Tell us more about your training</p>
-      <p className="text-center">Complete a short questionnaire to identify your injury risk</p>
+    <div className="relative min-h-screen">
+      <div className="text-center"><h1 className="text-center font-bold text-white capitalize rounded-xl bg-teal inline-block px-2 py-.1">{painAreaId}</h1></div>
+      <p className="text-teal font-bold text-center text-3xl">Tell us more about your training</p>
+      <p className="text-center text-xs">Complete a short questionnaire to identify your injury risk</p>
 
-      <form onSubmit={(e) => e.preventDefault()}>
+      <div className="pb-32">
         {questions.map((q, index) => (
-          <div className="font-bold text-center">
+          <div className="font-bold bg-light-gray rounded-xl p-6 mb-6 max-w-xl mx-auto flex flex-col gap-6">
             {q}
+            <input type="range" min={0} max={10} onChange={(e) => onChange(Number(e.target.value))} className="accent-teal w-full"/>
+            <div className="flex justify-between text-sm"> <span>{"None"}</span> <span>{10}</span> </div>
           </div>
         ))}
-
-        <button type="submit" className="font-bold">Get My Results</button>
-      </form>
+        
+      </div>
+        <button className="font-bold absolute right-0 bottom-0 whitespace-nowrap rounded-xl bg-teal inline-block px-2 py-.1 text-white">Get My Results</button>
     </div>
   );
 };
