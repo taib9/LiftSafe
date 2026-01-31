05a9efcdda4fbf8c3e8544fd5d027bc55daa682f
jimmytran1 <jimmytran935@gmail.com>
2026-01-30 18:52:36 -0700

    Removed Supabase test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import SupabaseTest from "./pages/SupabaseTest";
 import PainSelection from "./pages/PainSelection";
 import Results from "./pages/Results";
 import PainQuestionsPage from "./pages/PainQuestionsPage";
@@ -9,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/supabasetest" element={<SupabaseTest />} />
       <Route path="/pain-selection" element={<PainSelection />} />
       <Route path="/results" element={<Results />} />
       <Route path="/questions/:painAreaId" element={<PainQuestionsPage />} />
